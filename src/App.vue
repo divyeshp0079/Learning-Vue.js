d7a36ff0b4f0b8e2716ea828e04945d8c2c9b74f
Divyesh <divyesh.patel@metizsoft.com>
2022-12-13 17:15:18 +0530

    html binding

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,33 @@
   <!-- <firstComponents></firstComponents>
   <praticalComponents></praticalComponents>
   <eventClick></eventClick> -->
-  <getInput></getInput>
+  <!-- <getInput></getInput> -->
   <!-- <getcheckbox></getcheckbox> -->
   <!-- <ifElse></ifElse> -->
   <!-- <forLoop></forLoop> -->
+  <htmlbinding />
 </template>
 
 <script>
 // import firstComponents from "./components/firstComponents.vue";
 // import praticalComponents from "./components/praticalComponents.vue";
 // import eventClick from "./components/eventClick.vue";
-import getInput from "./components/getInput.vue";
+// import getInput from "./components/getInput.vue";
 // import getcheckbox from "./components/GetCheckbox&RadioButtonValue.vue";
 // import ifElse from "./components/ifElse.vue";
 // import forLoop from "./components/forLoop.vue";
+import htmlbinding from "./components/17_Html_binding.vue";
 export default {
   name: "App",
   components: {
     // firstComponents,
     // praticalComponents,
     // eventClick,
-    getInput,
+    // getInput,
     // getcheckbox,
     // ifElse,
     // forLoop,
+    htmlbinding,
   },
 };
 </script>
---
+++ src/components/17_Html_binding.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ text }}</h1>
+  {{ tag }}
+  <div class="htmldata" v-html="tag"></div>
+  <div v-html="pass"></div>
+</template>
+
+<script>
+export default {
+  name: "htmlBinding",
+  data() {
+    return {
+      text: "html text",
+      tag: "<h1>html add to content</h1>",
+      pass: "<h1>html data serve to content</h1>",
+    };
+  },
+};
+</script>
